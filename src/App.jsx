72cbe19d69938570825342d0932c2657bb316ef4
MattWate <matthew@dubyahinnovation.com>
2025-08-17 14:56:04 +0200

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+import React from 'react';
+import LoginPage from './pages/Login.jsx';
+
+export default function App() {
+  // We will add authentication and routing logic here later.
+  return (
+    <LoginPage />
+  );
+}
